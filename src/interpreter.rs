86c96b6636c6d9cd7b5cebe55bdd1366ef92bfcc
SIGMazer <mazinasd7@gmail.com>
2024-03-31 03:04:18 +0200

    refactor: main to interpreter

---
+++ src/interpreter.rs
@@ -0,0 +1,53 @@
+
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
+
+type Token = String;
+
+pub struct Interpreter {
+    pub had_error: bool,
+}
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Interpreter { had_error: false }
+    }
+
+    pub fn run(&mut self, source: String) {
+        let tokens: Vec<Token> = source.split_whitespace().map(|s| s.to_string()).collect();
+        println!("{:?}", tokens);
+    }
+
+    pub fn run_file(&mut self, path: &str) {
+        let file = File::open(path).expect("Failed to open file");
+        let bytes = BufReader::new(file).bytes();
+        self.run(bytes.map(|b| b.unwrap() as char).collect());
+        if self.had_error {
+            std::process::exit(65);
+        }
+
+    }
+
+    pub fn run_prompt(&mut self) {
+        loop {
+            print!("> ");
+            let _ = std::io::stdout().flush();
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).expect("Failed to read line");
+            // break if line is EOF
+            if line == "exit\n"{
+                break;
+            }
+            self.run(line);
+            self.had_error = false;
+        }
+    }
+
+    pub fn error(line: usize, message: &str) {
+        eprintln!("[line {}] Error: {}", line, message);
+    }
+
+    pub fn report(line: usize, loc: &str, message: &str) {
+        eprintln!("[line {}] Error {}: {}", line, loc, message);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,11 @@
 
+mod token;
+mod scanner;
+mod interpreter;
+
 use std::env::args;
-use std::fs::File;
-use std::io::{BufReader, Read};
-use std::io::Write;
+use interpreter::Interpreter;
 
-type Token = String;
-
-enum TokenType {
-    // Single-character tokens
-    LeftParen, RightParen, LeftBrace, RightBrace,
-    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
-
-    // One or two character tokens
-    Bang, BangEqual,
-    Equal, EqualEqual,
-    Greater, GreaterEqual,
-    Less, LessEqual,
-
-    // Literals
-    Identifier, String, Number,
-
-    // Keywords
-    And, Class, Else, False, Fun, For, If, Nil, Or,
-    Print, Return, Super, This, True, Var, While,
-
-    EOF,
-}
-
-struct Interpreter {
-    had_error: bool,
-}
-
-impl Interpreter {
-    fn new() -> Self {
-        Interpreter { had_error: false }
-    }
-
-    fn run(&mut self, source: String) {
-        let tokens: Vec<Token> = source.split_whitespace().map(|s| s.to_string()).collect();
-        println!("{:?}", tokens);
-    }
-
-    fn run_file(&mut self, path: &str) {
-        let file = File::open(path).expect("Failed to open file");
-        let bytes = BufReader::new(file).bytes();
-        self.run(bytes.map(|b| b.unwrap() as char).collect());
-        if self.had_error {
-            std::process::exit(65);
-        }
-
-    }
-
-    fn run_prompt(&mut self) {
-        loop {
-            print!("> ");
-            let _ = std::io::stdout().flush();
-            let mut line = String::new();
-            std::io::stdin().read_line(&mut line).expect("Failed to read line");
-            // break if line is EOF
-            if line == "exit\n"{
-                break;
-            }
-            self.run(line);
-            self.had_error = false;
-        }
-    }
-
-    fn error(&mut self, line: usize, message: &str) {
-        self.report(line, "", message);
-    }
-
-    fn report(&mut self, line: usize, loc: &str, message: &str) {
-        eprintln!("[line {}] Error {}: {}", line, loc, message);
-    }
-}
 
 
 fn main() {
